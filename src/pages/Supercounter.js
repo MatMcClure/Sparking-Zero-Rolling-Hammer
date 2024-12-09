d519ebb3685f60497b41cb27e71cc15f0eba1b5d
Matthew McClure <108655204+MatMcClure@users.noreply.github.com>
2024-12-09 02:03:56 -0500

    Add files via upload
    got rid of the search bar above the video and added some images

--- src/pages/Supercounter.js
+++ src/pages/Supercounter.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../style.css';
 import superCounterVid from '../videos/supercounttutVid.mp4';
+import superCounterOne from '../images/initial-counter.png';
+import superCounterTwo from '../images/supercounterhit.png';
+import superCounterThree from '../images/SCcontroller.png';
 
 function SuperCounters() {
   return (
@@ -12,12 +15,19 @@ function SuperCounters() {
           objectFit: 'cover',
           objectPosition: 'center',
           width: '80%',  // Adjust width as needed
-          marginTop: '-100px'
+          marginTop: '-100px',
+          border: 'solid',
+          borderRadius: '2%'
         }}
       >
-        <source src={superCounterVid} type="video/mp4" />
+        <source src={ superCounterVid } type="video/mp4" className="superCounterVid" />
         Your browser does not support the video tag.
       </video>
+      <div className="grid-container">
+        <img src={superCounterOne} alt="SuperCounterOne" className="superCounterOne" />
+        <img src={superCounterThree} alt="SuperCounterThree" className="superCounterThree" />
+        <img src={superCounterTwo} alt="SuperCounterTwo" className="superCounterTwo" />
+      </div>
     </main>
   );
 }
